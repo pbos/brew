e214002fca087a8e3c50826158704d27737b104b
Peter Boström <pbos@kth.se>
2012-12-09 20:22:16 +0100

    Recipe template

---
+++ template.rb
@@ -0,0 +1,17 @@
+Brew Name
+=============
+
+Brew Date: ____-__-__
+
+Target Volume: __L
+
+Ingredients
+-----------
+* _kg ____ Malt (Manifacturer)
+* __g ____ Hops
+* __g ____ Yeast
+
+Steps
+-----
+1. Grind the malt.
+...
